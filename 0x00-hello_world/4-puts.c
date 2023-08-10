bd4ffcf5c5950d323efbc84103520677625dec96
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-10 14:20:40 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/* c programm that prints prints exactly programming is like building a multillingual puzzle */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle/n")
-	return (0);
+puts("Programming is like building a multilingual puzzle,");
+return (0);
 }
+
