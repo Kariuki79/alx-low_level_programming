ee4f641d3108d18e0a8b4faf6716fb59d4c9fb11
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-10 13:52:21 -0700

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* c programm that prints prints exactly programming is like building a multillingual puzzle */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle/n")
+	return (0);
+}
