11ed22eb8f9d123305517767014c9c2799a2d073
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-10 14:27:32 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - main programme prints a file with puts function
+ * return - returns 0
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle,");
+puts("\"Programming is like building a multilingual puzzle,");
 return (0);
 }
 
