41e2fb9d71f44cb362bbc385cdfd86e8bf711dce
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-11 14:55:52 -0700

    update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,24 +6,24 @@
 
 int main(void)
 {
-        int i, j, k;
-
+	int i, j, k;
         for (i = 0; i < 9; i++)
-        {
-                for (j = i + 1; j <= 9; j++)
-                {
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
 			for (k = j + 1; k <= 9; k++)
 			{
-                        	putchar(i + '0');
-                        	putchar(j + '0');
-                        	if (i != 8)
-                        	{
-                                	putchar(',');
-                                	putchar(' ');
-                        	}
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-                }
-        }
-        putchar('\n');
-        return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
