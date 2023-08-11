d92e6f27abfe0050a35d2d886f6f07499b30dc27
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-11 13:39:43 -0700

    update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: 0
+*/
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		putchar(',');
+	       	putchar(' ');
+		putchar(i + '0');
+	}
+	
+	putchar('\n');
+	
+	return (0);
+}
