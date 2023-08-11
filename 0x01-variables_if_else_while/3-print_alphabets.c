6889feeba08a5c36cd3b0f053f6a250b6c7d9abc
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-11 11:45:48 -0700

    update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+
 /**
  * main - prints the alphabet in lowercase
  * Return: 0
-*/
+ */
 int main(void)
 {
-	char alpha;
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	{
-                putchar(alpha);
-        }
+    char alpha;
 
-	for (alpha = 'A'; alpha <= 'Z'; alpha++)
-	{
-                putchar(alpha);
-	}
-        putchar('\n');
-        return (0);
+    for (alpha = 'a'; alpha <= 'z'; alpha++)
+    {
+        putchar(alpha);
+    }
+
+    for (alpha = 'A'; alpha <= 'Z'; alpha++)
+    {
+        putchar(alpha);
+    }
+
+    putchar('\n');
+    return (0);
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+        	printf("%d\n", number);
+	}
+
+	printf('\n'); 
+
+	return 0;
+}
+
