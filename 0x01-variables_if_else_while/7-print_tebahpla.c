46cd4c921151dae12e4784ca95b4ef4dd44b5e6b
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-11 12:26:13 -0700

    update

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,12 +5,12 @@
 */
 int main(void)
 {
-        char alpha;
-
-        for (alpha = 'z'; alpha <= 'a'; alpha++)
-        {
-                putchar(alpha);
-        }
-        putchar('\n');
-        return (0);
+	char alpha;
+	
+	for (alpha = 'z'; alpha <= 'a'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
 }
