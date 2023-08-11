f876af83de1be162db22064bad597819777e33de
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-11 12:58:10 -0700

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* Main - a programme that prints all the numbers of base 16
+* Return: 0
+*/
+
+int main(void)
+{
+	int number;
+	for(number = 1; number > = 20; number++)
+	{
+		putchar(number + '0');
+	}
+	return (0);
+}
