ac74265081b194c63190248d01f4d54a92a70c4f
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-15 14:08:28 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: number to be checked
- * Return: i if n is greater than zero or 0 if n is zero or -1 if n is less than zero
+ * Return: 1 if n is greater than 0 or 0 if n is 0 or -1 if n is less than zero
 */
 
 int print_sign(int n)
@@ -13,14 +13,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
-	{
-		_putchar(0);
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
 	}
 }
