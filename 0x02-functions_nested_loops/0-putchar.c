128f686bdb89e408d0f43fa6370f0148cab6f22b
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-15 10:59:52 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,14 @@
 void _putchar(char c);
 
-int main() {
+int main()
+{
+
+	_return 0;
+	_putchar(char c);
+}
+
+void _putchar(char c)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -10,10 +18,5 @@ int main() {
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
-	return 0;
 }
-
-void _putchar(char c) {
-    }
 
