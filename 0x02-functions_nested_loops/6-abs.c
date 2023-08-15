ef77d627985f410e73b99811a1c169f2b28c3da1
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-15 14:54:48 -0700

    update

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 /**
  * _abs - computes the absolute value of an integer
  * @d: Number to be computed
- * Returns: 0
+ * Returns: value of d
 */
 
 int _abs(int d)
@@ -10,6 +10,7 @@ int _abs(int d)
 	if (d < 0)
 	{
 		int abs_value;
+
 		abs_value = d * -1;
 		return (abs_value);
 	}
