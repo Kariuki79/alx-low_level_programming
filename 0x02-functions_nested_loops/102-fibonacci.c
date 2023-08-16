d6808beb8ab290c3fd007e03e6a58b2c8bac3068
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-16 15:01:00 -0700

    update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,7 +2,6 @@
 
 /**
  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
- * followed by a new line
  * Return: 0 (Success)
 */
 int main(void)
@@ -10,7 +9,6 @@ int main(void)
 	long int a, b, c, next;
 	
 	b = 1;
-
 	c = 2;
 
 	for (a = 1; a <= 50; ++a)
