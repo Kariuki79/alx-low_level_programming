f0f9e21944049bda3f14e56280cb65a4535b7da4
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-16 14:57:20 -0700

    update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
-k#include <stdio.h>
+#include <stdio.h>
 /**
-* main - list natural numbers that are multiples of 3 or  5
-* Return: 0
-*/
+ * * main - list natural numbers that are multiples of 3 or  5
+* * Return: 0
+* */
+
 int main(void)
 {
 	int j, k = 0;
@@ -18,24 +18,3 @@ int main(void)
 	printf("%d\n", k);
 	return(0);
 }
-=======
-#include <stdio.h>
-/**
-* main - list natural numbers that are multiples of 3 or  5
-* Return: 0
-*/
-int main(void)
-{
-        int j, k = 0;
-        while (j < 1024)
-        {
-                if ((j % 3 == 0) || (j % 5 == 0))
-                {
-                        k += j;
-                }
-                j++;
-        }
-        printf("%d\n", k);
-        return(0);
-}
->>>>>>> 52d2c2c1adaf7cda0a92e5b27d8dc91a14bc1432
