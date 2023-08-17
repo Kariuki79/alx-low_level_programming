5e4adf724b6d2f2ef1637b561d5d16baa404ce2e
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-17 06:17:02 -0700

    update

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers from 0 to 9 apart from 2 and 4
+ * @i: stores the numbers;
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+		
+	for (i = 0; i <= 9; i++)
+	{
+		if(i != 2; && i != 4)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
