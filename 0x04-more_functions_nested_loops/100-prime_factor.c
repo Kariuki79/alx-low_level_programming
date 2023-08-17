e10345f8403f497076e711efdcbcfbdb6a2357e9
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-17 11:30:20 -0700

    update

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - largest prime facter of 612852475143
+ * Return: 0
+*/
+int main(void)
+{
+	long int x, i, prime_factor
+
+	prime_factor= -1;
+	x = 612852475143;
+
+	while (x % 2 == 0)
+	{
+		prime_factor = 2;
+		x = x / 2;
+	}
+	for (i = 3; i <= x / 2; i = i + 2)
+	{
+		while (x % i == 0)
+		{
+			prime_factor = i;
+			x = x / i;
+		}
+	}
+	if (x > 2)
+		prime_factor = x;
+
+	printf("%ld\n", prime_fzctor);
+	return (0);
+}
