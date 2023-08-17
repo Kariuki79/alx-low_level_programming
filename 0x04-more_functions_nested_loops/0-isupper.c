c27ebfda3c6fece21963b83914fc550bf594a692
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-17 10:07:49 -0700

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *  _isupper - Checks for uppercase characters
+ * _isupper - Checks for uppercase characters
  * @c: character to be checked 
  * Return: 1 if c is uppercase 0 otherwise
 */
