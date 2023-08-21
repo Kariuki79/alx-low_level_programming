dcd41a7be0f8805dc7dde5ff4658fa6ea58a14b7
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-21 16:32:26 -0700

    update

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array
+ * @a: pointer to the array
+ * @n: length of the array
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[index]);
+		if (i != (n - ))
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	_putchar('\n');
+}
