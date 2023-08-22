b169765ad64a09a1c42e9c7a5eea840d36055188
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-22 01:05:07 -0700

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,30 +18,36 @@ int _atoi(char *s)
 	p = 0;
 	numb = 0;
 
-	while (s[len] != '\0')
-		len++;
-
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 	while (a < length && p == 0)
 	{
 		if (s[a] == '-')
+		{
 			++j;
-
+		}
 		if (s[a] >= '0' && s[a] <= '9')
 		{
 			numb = s[a] - '0';
 			if (j % 2)
+			{
 				numb = -numb;
+			}
 			m = m * 10 + numb;
 			p = 1;
 			if (s[a + 1] < '0' || s[a + 1] > '9')
+			{
 				break;
+			}
 			p = 0;
 		}
 		a++;
 	}
-
 	if (p == 0)
+	{
 		return (0);
-
+	}
 	return (m);
 }
