bb1a7ce1f10593d8f98e016929d86d5073fc5d01
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-22 12:34:28 -0700

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,47 +9,24 @@
 
 int _atoi(char *s)
 {
-	int a, j, m, length, p, numb;
+	int a = 0, j = 0, m = 0, length = 0, p = 0, numb = 0;
 
-	a = 0;
-	j = 0;
-	m = 0;
-	length = 0;
-	p = 0;
-	numb = 0;
-
-	while (s[length] != '\0')
-	{
+	while (s[length])
 		length++;
-	}
-	for (j = 0; j < a; j++)
-		while (a < length && p == 0)
-		{
-			if (s[a] == '-')
-			{
-				++j;
-			}
-			if (s[a] >= '0' && s[a] <= '9')
-			{
-				numb = s[a] - '0';
-				if (j % 2)
-				{
-					numb = -numb;
-				}
-				m = m * 10 + numb;
-				p = 1;
-				if (s[a + 1] < '0' || s[a + 1] > '9')
-				{
-					break;
-				}
-				p = 0;
-			}
-			a++;
-		}
-	if (p == 0)
+	while (a < length && p == 0)
 	{
-		return (0);
+		if (s[a] == '-')
+			++j;
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			numb = s[a] - '0';
+			m = m * 10 + (j % 2 ? -numb : numb);
+			p = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+		}
+		a++;
 	}
-	return (m);
+	return (p ? m : 0);
 }
 
