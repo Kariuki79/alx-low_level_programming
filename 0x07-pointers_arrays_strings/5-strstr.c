7984ec98783cfe0a9c789ab37a7624ccb7b49e66
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-29 12:22:17 -0700

    update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,7 +11,7 @@ char *_strstr(char *haystack, char *needle)
 
 	while (*haystack != '\0')
 	{
-		s1 = haystack; 
+		s1 = haystack;
 		s2 = needle;
 		while (*haystack != '\0' && *s2 != '\0' && *haystack == *s2)
 		{
