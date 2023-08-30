ca714dd44c6418ccf47616609b4efed808f11b46
Kariuki79 <kariukimwangi75@gmail.com>
2023-08-30 13:36:08 -0700

    update

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,17 +2,18 @@
 /**
  * factorial - function that returns the factorial of a given number
  * @n: number to calculate the factorial
- * Return: factorial of number n
+ * Return: factorial of number @n
  */
 int factorial(int n)
 {
 	if (n < 0)
 	{
-		return (-1);
+		return (-1); /* Return -1 indicating an error for negative n */
 	}
 	if (n == 0)
 	{
-		return (1);
+		return (1);  /* A base case: factorial of 0 is 1 */
 	}
+
 	return (n * factorial(n - 1));
 }
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns the value of x raised to power y
+ * @x: value you to be raised by power y
+ * @y: power
+ * Return: -1 if y is lower than 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return -1;
+	}
+	return x * _pow_recursion(y - 1);
+}
