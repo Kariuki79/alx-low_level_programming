26d66ef7fff1d1d6a8a32b8e544f98f67d084112
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-05 05:01:52 -0700

    update

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - searches a string for any of
+ * a set of bytes
+ * @s: string
+ * @accept: bytes in the string
+ * Return: pointer to the byte  in s or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int j = 0;
+
+	while (*s != '\0')
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @d: Number to be computed
+ * Return: Absolute value of d
+*/
+
+int _abs(int d)
+{
+	if (d < 0)
+	{
+		int abs_value;
+
+		abs_value = d * -1;
+		return (abs_value);
+	}
+	return (d);
+}
