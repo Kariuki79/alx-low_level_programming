88cbe2c1dd489e1506fe5bcafb0a1aaedda5622d
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-06 09:18:45 -0700

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,16 +13,18 @@ int main(int argc, char *argv[])
 
 	if (argc < 2)
 	{
-		printf("%d", 0);
+		printf("%d\n", 0);
+		return (0);
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		int num = atoi(argv[i]);
+		if (num <= 0)
 		{
-			printf("%s\n", "Error");
+			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[i]);
+		sum += num;
 	}
 	printf("%d\n", sum);
 	return (0);
