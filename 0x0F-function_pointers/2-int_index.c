8d177a1c701dd0729500a58dbbc586db32995132
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-13 12:41:06 -0700

    update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,6 +4,7 @@
  * @size: number of elements in the array array
  * @array: array
  * @cmp: pointer to the function that will do comparison
+ * Return: -1 if size is less than 0, return i return -1 if no element matches
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -13,12 +14,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	for (i = 0; i <size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 		{
-			return i;
+			return (i);
 		}
 	}
-	return -1;
+	return (-1);
 }
