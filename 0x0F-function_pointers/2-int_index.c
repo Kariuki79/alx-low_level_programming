82d100f1b1e693022426dfee1bd51af3b28349fb
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-13 12:47:53 -0700

    update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,20 +10,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
+	if (array && cmp)
 	{
-		return;
-	}
-
-	if (size <= 0)
-	{
-		return (-1);
-	}
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
+		if (size <= 0)
 		{
-			return (i);
+			return (-1);
+		}
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
