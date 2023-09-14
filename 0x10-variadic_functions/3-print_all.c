39c9f8d1f3893b2a44b4f91b62e48f54d7325852
Kariuki Mwangi <139071098+Kariuki79@users.noreply.github.com>
2023-09-14 23:08:20 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,42 @@
 #include <stdio.h>
 #include <stdarg.h>
-/**
- * print_all - prints anything
- * @format: a list of types of arguments
- */
+
 void print_all(const char * const format, ...)
 {
-	va_list args;
+    if (format == NULL)
+    {
+        printf("\n");
+        return;
+    }
 
-	char *separator = "";
+    va_list args;
+    va_start(args, format);
 
-	unsigned int i = 0;
+    const char *separator = "";
+    char format_specifier;
+    
+    for (int i = 0; format[i]; i++)
+    {
+        format_specifier = format[i];
+        
+        if (format_specifier == 'c')
+            printf("%s%c", separator, va_arg(args, int));
+        else if (format_specifier == 'i')
+            printf("%s%d", separator, va_arg(args, int));
+        else if (format_specifier == 'f')
+            printf("%s%f", separator, va_arg(args, double));
+        else if (format_specifier == 's')
+        {
+            char *str = va_arg(args, char *);
+            if (str == NULL)
+                str = "(nil)";
+            printf("%s%s", separator, str);
+        }
+        
+        separator = ", ";
+    }
 
-	va_start(args, format);
+    va_end(args);
 
-	while (format && format[i])
-	{
-		switch (format[i])
-		{
-			case 'c':
-				printf("%s%c", separator, va_arg(args, int));
-				break;
-			case 'i':
-				printf("%s%d", separator, va_arg(args, int));
-				break;
-			case 'f':
-				printf("%s%f", separator, va_arg(args, double));
-				break;
-			case 's':
-				{
-					char *str = va_arg(args, char *);
-
-					if (str == NULL)
-					{
-						str = "(nil)";
-					}
-					printf("%s%s", separator, str);
-				}
-				break;
-			default:
-				break;
-		}
-		separator = ", ";
-		i++;
-	}
-	va_end(args);
-	printf("\n");
+    printf("\n");
 }
