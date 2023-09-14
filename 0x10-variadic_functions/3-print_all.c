6e552ce3172508c4690dd39d08917eabe8ba902e
Kariuki Mwangi <139071098+Kariuki79@users.noreply.github.com>
2023-09-14 23:18:47 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * print_all - prints anything
+ * @format: a list of types of arguments
+ */
 void print_all(const char * const format, ...)
 {
      va_list args;
     va_start(args, format);
-     const char *separator = "";
+     char *separator = "";
     char format_specifier;
 
     if (format == NULL)
     {
         printf("\n");
-        return;
     }
     
     for (i = 0; format[i]; i++)
@@ -27,6 +30,7 @@ void print_all(const char * const format, ...)
         else if (format_specifier == 's')
         {
             char *str = va_arg(args, char *);
+             
             if (str == NULL)
                 str = "(nil)";
             printf("%s%s", separator, str);
