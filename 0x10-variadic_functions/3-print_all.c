2313395a09c861e59e89ce3bb78dc7ed90a494aa
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-14 13:30:55 -0700

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,45 +1,43 @@
 #include <stdio.h>
 #include <stdarg.h>
-
 /**
  * print_all - prints anything
  * @format: a list of types of arguments
  */
 void print_all(const char * const format, ...)
 {
-     va_list args;
-    va_start(args, format);
-     char *separator = "";
-    char format_specifier;
+	va_list args;
 
-    if (format == NULL)
-    {
-        printf("\n");
-    }
-    
-    for (i = 0; format[i]; i++)
-    {
-        format_specifier = format[i];
-        
-        if (format_specifier == 'c')
-            printf("%s%c", separator, va_arg(args, int));
-        else if (format_specifier == 'i')
-            printf("%s%d", separator, va_arg(args, int));
-        else if (format_specifier == 'f')
-            printf("%s%f", separator, va_arg(args, double));
-        else if (format_specifier == 's')
-        {
-            char *str = va_arg(args, char *);
-             
-            if (str == NULL)
-                str = "(nil)";
-            printf("%s%s", separator, str);
-        }
-        
-        separator = ", ";
-    }
+	va_start(args, format);
 
-    va_end(args);
+	char *separator = "";
 
-    printf("\n");
+	char format_specifier;
+
+	if (format == NULL)
+	{
+		printf("\n");
+	}
+	for (i = 0; format[i]; i++)
+	{
+		format_specifier = format[i];
+
+		if (format_specifier == 'c')
+			printf("%s%c", separator, va_arg(args, int));
+		else if (format_specifier == 'i')
+			printf("%s%d", separator, va_arg(args, int));
+		else if (format_specifier == 'f')
+			printf("%s%f", separator, va_arg(args, double));
+		else if (format_specifier == 's')
+		{
+			char *str = va_arg(args, char *);
+
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s%s", separator, str);
+		}
+		separator = ", ";
+	}
+	va_end(args);
+	printf("\n");
 }
