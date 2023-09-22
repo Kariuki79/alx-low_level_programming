61a7c447f0d9c6ff3900beaef273d436098b61f6
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-21 19:24:26 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * function is executed
  * Return: 0
  */
-void first(void)__attribute__((constructor));
+void __attribute __((constructor));
 {
 	printf("You're beat! and yet, you must allow");
 	printf("I bore my house upon my back!\n");
