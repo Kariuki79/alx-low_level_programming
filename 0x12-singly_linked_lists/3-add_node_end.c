cb10779713420f029a1157085b4dab5e2fdd529c
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-21 19:19:53 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -31,7 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	else
 	{
-		while (current ->next != NULL)
+		while (current->next != NULL)
 			current = current->next;
 		current->next = new_node;
 	}
