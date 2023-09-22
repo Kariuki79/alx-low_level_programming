fbeab8e3fb42a5e3fdce0932112ef010a9502202
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-21 17:38:53 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ * Return: 0
+ */
+void first(void)__attribute__((constructor));
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
