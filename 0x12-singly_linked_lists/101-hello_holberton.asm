8b34190c34fa66c8dc02408c453b985f0de1045a
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-22 18:47:53 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,9 @@
-section .data
-format db 'Hello, Holberton',10,0  ; Define the format string with a newline and null terminator
-	section .text
-	global main
-	extern printf
-
+ global    main
+          extern    printf
 main:
-	push format	; Push the address of the format string onto the stack
-	call printf	; Call the 'printf' function with the address of the format string on the stack
-	add esp, 4; Clean up the stack (remove the address pushed earlier)
-xor eax, eax      ; Set EAX to 0 (return value for main)
-	ret
-
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
