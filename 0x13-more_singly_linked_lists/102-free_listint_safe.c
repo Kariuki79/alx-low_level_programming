38c87b575f0f27aa9c72e3d5dfd0f16974d4d31f
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-26 14:11:45 -0700

    update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,40 +1,62 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _ra - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list
+ * @size: size of the new list
+ * @new: new node to add to the list
+ * Return: pointer to the new list
+ */
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **newl;
+	size_t i;
 
+	newl = malloc(size * sizeof(listint_t *));
+	if (newl == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newl[i] = list[i];
+	newl[i] = new;
+	free(list);
+	return (newl);
+}
 /**
  * free_listint_safe - safely frees a linked list and counts the elements
  * @head_ptr: a pointer to a pointer to the head of the linked list.
  *
  * Return: the number of elements that were freed.
  */
-size_t free_listint_safe(listint_t **head_ptr)
+size_t free_listint_safe(listint_t **head)
 {
-	size_t elements_freed = 0;
-	int address_diff;
-	listint_t *current_node;
+	size_t a, num = 0;
+	listint_t **list = NULL;
+	listint_t *next;
 
-	if (!head_ptr || !*head_ptr)
-		return (0);
-
-	while (*head_ptr)
+	if (head == NULL || *head == NULL)
+		return (num);
+	while (*head != NULL)
 	{
-		address_diff = *head_ptr - (*head_ptr)->next;
-		if (address_diff > 0)
+		for (a = 0; a < num; a++)
 		{
-			current_node = (*head_ptr)->next;
-			free(*head_ptr);
-			*head_ptr = current_node;
-			elements_freed++;
+			if (*head == list[a])
+			{
+				*head = NULL;
+				free(list);
+				return (num);
+			}
 		}
-		else
-		{
-			free(*head_ptr);
-			*head_ptr = NULL;
-			elements_freed++;
-			break;
-		}
+		num++;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
-
-	*head_ptr = NULL;
-
-	return (elements_freed);
+	free(list);
+	return (num);
 }
