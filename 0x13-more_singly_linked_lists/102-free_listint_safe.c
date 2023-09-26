b783e9fc6987249b3d166a4a6f2ddefb86bbcdb3
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-26 14:57:27 -0700

    update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,42 +2,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _ra - reallocates memory
+ * @list: the old list to append
+ * @size: size of the new list
+ * @new: new node to add to the list
+ * Return: pointer to the new list
+ */
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **newl;
+	size_t a;
+
+	newl = malloc(size * sizeof(listint_t *));
+	if (newl == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (a = 0; a < size - 1; a++)
+		newl[a] = list[a];
+	newl[a] = new;
+	free(list);
+	return (newl);
+}
+/**
  * free_listint_safe - safely frees a linked list and counts the elements
  * @h: a pointer
  * Return: the size of the list
  */
-size_t free_listint_safe(listint_t **h)
+size_t free_listint_safe(listint_t **head)
 {
-	size_t counter = 0;
-	listint_t *temp;
+	size_t i, number = 0;
+	listint_t **list = NULL;
+	listint_t *next;
 
-	temp = *h;
-	while (temp)
+	if (head == NULL || *head == NULL)
+		return (number);
+	while (*head != NULL)
 	{
-		temp = *h;
-		temp = temp->next;
-		free_list(temp);
-		counter++;
+		for (i = 0; i < number; i++)
+		{
+			if (*head == list[i])
+			{
+				*head = NULL;
+				free(list);
+				return (number);
+			}
+		}
+		number++;
+		list = _ra(list, number, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
-	*h = NULL;
-
-	return (counter);
-}
-/**
- * free_list - frees a listint_t recursively
- * @head: A pointer to the listint_t structure
- * Return: Nothing
- */
-void free_list(listint_t *head)
-{
-	listint_t *t;
-
-	if (head)
-	{
-		t = head;
-		t = t->next;
-		free(t);
-		free_list(t);
-	}
-	free(head);
+	free(list);
+	return (number);
 }
