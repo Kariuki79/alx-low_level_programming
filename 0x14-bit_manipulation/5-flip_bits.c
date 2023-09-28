3e83233e57bf3ff29e8f4ec9d0a181f93f3eedbe
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-28 12:13:25 -0700

    update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,7 +4,8 @@
  * to get from one number to another
  * @n: first number
  * @m: second number
- * Return: number of bits you would need to flip to get from one number to another
+ * Return: number of bits you would need to flip to get
+ * from one number to another
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
