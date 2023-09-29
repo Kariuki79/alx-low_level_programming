a0a7c1d46cb99a3b17187d8ee8ab8c5835f4a3d3
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-29 14:51:00 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,15 +6,17 @@
  * binary representation of a number
  * @index: unsigned integer representing the index of the bit to be
  * retrieved
+ * @n: numbers to search
  * Return:  value of the extracted bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-        int bit_value;
-        
-        if (index >= 64)
-                return (-1);
-        bit_value = (n >> index) & 1;
-        
-        return (bit_value);
+	int bit_value;
+
+	if (index >= 64)
+
+		return (-1);
+	bit_value = (n >> index) & 1;
+
+	return (bit_value);
 }
