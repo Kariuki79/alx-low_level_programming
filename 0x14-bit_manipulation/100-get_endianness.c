78e7e1fa1d3ae8e9ce8cf3259ab6d0d4b3aec8c4
Kariuki79 <kariukimwangi75@gmail.com>
2023-09-29 15:27:24 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,19 +1,21 @@
 #include "main.h"
+
 /**
  * get_endianness -  checks the endianness
  * Return: 0 if big endian 1 if little endian
-*/
+ */
 int get_endianness(void)
 {
-        unsigned int value = 1;
-        unsigned char *byte_ptr = (unsigned char *)&value;
+	unsigned int value = 1;
+	unsigned char *byte_ptr = (unsigned char *)&value;
 
-        if (*byte_ptr == 1)
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (*byte_ptr == 1)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
 }
