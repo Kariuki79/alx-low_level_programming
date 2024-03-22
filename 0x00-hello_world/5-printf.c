ed2c29e3ed08a40cdf88c80e199e19cc4c87bd44
Kariuki79 <kariukimwangi75@gmail.com>
2024-03-22 06:36:00 -0700

    C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * A program that prints a string of line
+ * Returns 0
+ */
+int main()
 {
 	printf("%s\n", "with proper grammar, but the outcome is a piece of art");
 	return (0);
