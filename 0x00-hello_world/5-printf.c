5851e099547eee8f6ca4805acd473603556e94da
Kariuki79 <kariukimwangi75@gmail.com>
2024-03-22 06:46:52 -0700

    C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
+ * main - Entry point
+ *
  * A program that prints a string of line
- * Returns 0
+ *
+ * Return: 0 upon successul completion
  */
-int main()
+int main(void)
 {
-	printf("%s\n", "with proper grammar, but the outcome is a piece of art");
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
