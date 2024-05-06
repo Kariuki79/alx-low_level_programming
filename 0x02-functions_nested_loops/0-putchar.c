932262c386f802c26158753850052773afcfc878
Kariuki79 <kariukimwangi75@gmail.com>
2024-05-06 02:20:32 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description : A program that prints a new _putchar, followed by a new line
+ *
+ * Return: 0 (sucess)
+ */
 
 int main(void)
 {
 	printf("_putchar\n");
-	return(0);
+
+	return (0);
 }
