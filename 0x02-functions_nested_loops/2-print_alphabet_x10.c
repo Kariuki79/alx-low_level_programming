a528c0e8a57d4b1cc8944e197efbfb66126cb071
Kariuki79 <kariukimwangi75@gmail.com>
2024-05-06 04:09:45 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int _putchar(char c);
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times, followed
+ * by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char m;
+	int q;
+
+	for (q = 0; q < 10; q++)
+	{
+		for (m = 'a'; m <= 'z'; m++)
+		{
+			_putchar(m);
+		}
+		_putchar('\n');
+	}
+}
