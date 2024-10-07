3d518af2de3633913e96848ebab3f3331e4ac5d0
Kariuki79 <kariiukimwangi75@gmail.com>
2024-10-07 12:18:09 -0700

    correction of code

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,14 +1,63 @@
 #include <stdio.h>
 #include <ctype.h>
-/**
- * print_buffer - prints a buffer
- * @b: points buffer
- * @size: size of buffer
- */
 
+/**
+ * print_hex_part - Prints the hexadecimal part of the buffer.
+ * @b: The pointer to the buffer.
+ * @i: The current index in the buffer.
+ * @size: The total size of the buffer.
+ */
+void print_hex_part(char *b, int i, int size)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		if (i + j < size)
+		{
+			printf("%02x", (unsigned char)b[i + j]);
+			if (j % 2 == 1)
+				printf(" ");
+		}
+		else
+		{
+			printf("   ");
+		}
+	}
+}
+
+/**
+ * print_ascii_part - Prints the ASCII part of the buffer.
+ * @b: The pointer to the buffer.
+ * @i: The current index in the buffer.
+ * @size: The total size of the buffer.
+ */
+void print_ascii_part(char *b, int i, int size)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		if (i + j < size)
+		{
+			char c = b[i + j];
+
+			if (isprint((unsigned char)c))
+				printf("%c", c);
+			else
+				printf(".");
+		}
+	}
+}
+
+/**
+ * print_buffer - Prints a buffer in both hex and ASCII formats.
+ * @b: The pointer to the buffer.
+ * @size: The size of the buffer.
+ */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int i;
 
 	if (size <= 0)
 	{
@@ -18,40 +67,17 @@ void print_buffer(char *b, int size)
 
 	for (i = 0; i < size; i += 10)
 	{
-		printf("%08x ", i);
-		for (j = 0; j < 10; j++)
-		{
-			if (i + j < size)
-			{
-				printf("%02x", (unsigned char)b[i + j]);
-				if (j % 2 == 1)
-				{
-					printf(" ");
-				}
-			}
-			else
-			{
-				printf("   ");
-			}
-		}
-		printf(" ");
-		for (j = 0; j < 10; j++)
-		{
-			if (i + j < size)
-			{
-				char c = b[i + j];
+		/* Print the starting position in hex (8 chars) */
+		printf("%08x: ", i);
 
-				if (isprint(c))
-				{
-					printf("%c", c);
-				}
+		/* Print the hex part */
+		print_hex_part(b, i, size);
 
-				else
-				{
-					printf(".");
-				}
-			}
-		}
+		/* Print the ASCII part */
+		print_ascii_part(b, i, size);
+
+		/* Newline after each line */
 		printf("\n");
 	}
 }
+
