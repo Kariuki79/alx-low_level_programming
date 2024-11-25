1421778eb1807fb0f6a8cdcaa86d8cade8cc395d
Kariuki79 <kariiukimwangi75@gmail.com>
2024-11-25 15:41:37 -0800

    Add script to create static library liball.a

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# Compile all .c files in the current directory into .o files
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c
-# Create a static library liball.a from all .o files
+# A script to create a static library called liball.a from all .c files in the current directory
+
+# Compile all .c files into .o files
+gcc -c *.c
+
+# Create the static library liball.a from the .o files
 ar rcs liball.a *.o
+
+# Clean up: Remove the .o files
+rm -f *.o
