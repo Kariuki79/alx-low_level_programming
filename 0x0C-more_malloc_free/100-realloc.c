2e89c1af135cb50d8a0bc9649de9ded3dbac02c2
Kariuki79 <kariukimwangi75@gmail.com>
2025-03-20 13:22:57 -0700

    function that reallocates a memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
-#include "main.h"
 
 /**
- * _realloc - Reallocates memory block using malloc and free
- * @ptr: Pointer to the previous allocated memory block
- * @old_size: Size in bytes of the allocated memory block
+ * _realloc - Reallocates a memory block using malloc and free
+ * @ptr: Pointer to the previously allocated memory block
+ * @old_size: Size in bytes of the allocated space for ptr
  * @new_size: New size in bytes of the new memory block
  *
  * Return: Pointer to the newly allocated memory, or NULL on failure
@@ -15,13 +14,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *new_ptr;
 	unsigned int copy_size;
 
+	/* If new_size is zero and ptr is not NULL, equivalent to free(ptr) */
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	/* if ptr is NULL, equilavent to malloc(new_size) */
+	/* If ptr is NULL, equivalent to malloc(new_size) */
 	if (ptr == NULL)
 		return (malloc(new_size));
 
@@ -34,7 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_ptr == NULL)
 		return (NULL);
 
-	/* Copy data from old block to new block,upto min(old_size, new_size) */
+	/* Copy data from old block to new block, up to min(old_size, new_size) */
 	copy_size = (old_size < new_size) ? old_size : new_size;
 	memcpy(new_ptr, ptr, copy_size);
 
